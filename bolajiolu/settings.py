521192c26f6d018670c6213590bbd7cda1661e06
bolajio <108666295+Bolaji18@users.noreply.github.com>
2024-01-26 00:57:39 -0800

    Update settings.py

--- bolajiolu/settings.py
+++ bolajiolu/settings.py
@@ -130,8 +130,8 @@ LOGIN_URL = 'log'
 
 LOGOUT_URL = reverse_lazy('logout')
 STATIC_URL = '/static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 # Base url to serve media files
 MEDIA_URL = '/images'
