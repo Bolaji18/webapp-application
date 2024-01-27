f600881844e604e26677e36ef178ede6538f7f15
bolajio <108666295+Bolaji18@users.noreply.github.com>
2024-01-27 00:09:45 -0800

    Update settings.py

--- home2b/settings.py
+++ home2b/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Base url to serve media files
 MEDIA_URL = '/images/'
