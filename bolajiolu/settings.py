2fc2b615ce73a850eb37ccc13740f2934c256c95
bolaji daropale <108666295+Bolaji18@users.noreply.github.com>
2025-09-27 07:35:24 +0100

    Update settings.py

--- bolajiolu/settings.py
+++ bolajiolu/settings.py
@@ -119,12 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'trustallianceng@gmail.com'
-EMAIL_HOST_PASSWORD ='dnie umop lrrh asuc'
 
 LOGIN_URL = 'log'
 # Static files (CSS, JavaScript, Images)
